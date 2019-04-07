e580fdf3597a29ef119c0d382a09cffb38c379b3
PetraSChka <43903177+PetraSChka@users.noreply.github.com>
2019-04-07 15:50:08 +0300

    Update 5-1.cpp

--- 5-1.cpp
+++ 5-1.cpp
@@ -1,5 +1,6 @@
+//Горизонтальный градиент
 #include<iostream>
-#include<fstream>  //где условие задачи? Какой градиент ты делаешь???
+#include<fstream>  
 using namespace std;
 struct color { unsigned char r; unsigned char g; unsigned char b; } c;
 
@@ -8,12 +9,21 @@ int main() {
 	ofstream out("d:\\result.bmp", ios::binary);
 	if (!in) { cout << "No file d:\\1.bmp. Can't open\n"; exit(1); }
 	if (!out) { cout << "No file d:\\result.bmp. Can't create\n"; exit(1); }
-// и что? даже заголовок не прочитаем-не запишем ? 
+
 	char buf[30];
 	unsigned int w, h;
 	int tmp;
 
 	c.r=c.g=c.b=0;
+
+	in.read((char *)&buf, 18);
+	out.write((char *)&buf, 18);
+	in.read((char *)&w, 4);
+	in.read((char *)&h, 4);
+	out.write((char *)&w, 4);
+	out.write((char *)&h, 4);
+	in.read((char *)&buf, 28); 
+	out.write((char *)&buf, 28);
 
 	tmp = w / 255 + 1;
 
