f177da767abb56b6a595ecb8c347c5bee17ddfab
PetraSChka <43903177+PetraSChka@users.noreply.github.com>
2019-04-27 23:07:07 +0300

    Update 6-1.cpp

--- 6-1.cpp
+++ 6-1.cpp
@@ -1,4 +1,4 @@
-﻿//начало великого
+//начало великого
 #include <iostream>
 #include <ctime>
 using namespace std;
@@ -22,40 +22,83 @@ public:
 			Arr[i][j] = rand() % 4;
 	}
 	
-	friend ostream& operator<< (ostream &out, const Matrixs &point);
+	friend ostream& operator<< (ostream &out, const Matrixs &mat) { //вывод
+		for (int i = 0; i < 2; ++i) {
+			for (int j = 0; j < 2; ++j)
+				out << mat.Arr[i][j] << " ";
+			out << endl;
+		}
+		return out;
+	}
+
+	friend istream& operator >> (istream &in, Matrixs &mat) { //ввод
+		for (int i = 0; i < 2; ++i) {
+			for (int j = 0; j < 2; ++j) {
+				cout << "Enter element of " << i << " stroki i " << j << " stolbtsa: ";
+				in >> mat.Arr[i][j];
+				cout << endl;
+			}
+		}
+		return in;
+	}
+
+	Matrixs operator=(Matrixs &mat) { //присвоить
+		for (int i = 0; i < 2; ++i)
+			for (int j = 0; j < 2; ++j)
+				Arr[i][j] = mat.Arr[i][j];
+		return *this;
+	}
+
+	Matrixs operator+(Matrixs &mat) {
+		for (int i = 0; i < 2; ++i)
+			for (int j = 0; j < 2; ++j)
+				Arr[i][j] += mat.Arr[i][j];
+		return *this;
+	}
+
+	Matrixs operator-(Matrixs &mat) {
+		for (int i = 0; i < 2; ++i)
+			for (int j = 0; j < 2; ++j)
+				Arr[i][j] -= mat.Arr[i][j];
+		return *this;
+	}
 
 private:
 	const int M=2, N=2; //стороны матрицы
 	int Arr[2][2];
 };
 
-ostream& operator<< (ostream &out, const Matrixs &mat)
-{
-	for (int i = 0; i < 2; ++i)
-		for (int j = 0; j < 2; ++j)
-			out << mat.Arr[i][j] << " ";
-	cout << endl;
-
-	return out;
-}
-
-
 int main()
 {
 	setlocale(LC_ALL, "RUS");
 
+	cout << "1 Матрица" << endl;
 	Matrixs matrix_1;
 	matrix_1.InitMatrix();
 	matrix_1.GetMatrix();
 	cout << endl;
 
+	cout << "2 Матрица" << endl;
 	Matrixs matrix_2;
 	matrix_2.InitMatrix();
 	matrix_2.GetMatrix();
 	cout << endl;
 
-	cout << matrix_2;
+	cout << "Сложили 1 со 2" << endl;
+	matrix_2 = matrix_2 + matrix_1;
+	cout << matrix_2 << endl;
+
+	cout << "Отняли 1 от 2" << endl;
+	matrix_2 = matrix_1 - matrix_2;
+	cout << matrix_2 << endl;
 	
+	cout << "2 Матрице присвоили 1 матрицу" << endl;
+	matrix_2 = matrix_1;
+	cout << matrix_2 << endl;
+	
+	cin >> matrix_2;
+	cout << matrix_2 << endl;
+
 	system("pause");
 	return 0;
 }
